f91323c373181ba11cdff09194f549f4287c4591
mohammad <9m.kh557@gmai.com>
2024-03-28 12:10:48 +0330

    c2

--- src/App.java
+++ src/App.java
@@ -1,4 +1,5 @@
 
+import i2.act.main.FuzzPEG;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
@@ -14,26 +15,26 @@ import parser.*;
 public class App
 {
     public static void main( String[] args ) throws IOException {
-//        CharStream in = CharStreams.fromFileName("resources/example.java");
-//        Java8Lexer lexer = new Java8Lexer(in);
-//        CommonTokenStream tokens = new CommonTokenStream(lexer);
-//        Java8Parser parser = new Java8Parser(tokens);
-//        ParseTree parseTree = parser.compilationUnit();
-//
-//        // listener
-//        TestListener listener = new TestListener(0, 10);
-//        new ParseTreeWalker().walk(listener, parseTree);
-//
-//        ParserRuleContext mutationTar = listener.result.get(new Random().nextInt(listener.result.size()));
-////        for(ParserRuleContext a : listener.result) {
-////            System.out.println(a.getText());
-////        }
-//        System.out.println();
-////        mutationTar.getRuleContext()
+        ParseTree entireTree = getParseTree("resources/example.java");
 
-        String[] newArgs = {"--grammar", "resources/fuzz_peg_grammar.txt"};
+        NodeSelector listener = new NodeSelector(0, 10);
+        new ParseTreeWalker().walk(listener, entireTree);
 
-//        FuzzPEG.main(newArgs);
-        new AntlrToFuzzpegGrammar().convert("resources/fuzz_peg_grammar.txt");
+        ParserRuleContext mutationTar = listener.result.get(new Random().nextInt(listener.result.size()));
+
+        for(ParserRuleContext a : listener.result) {
+            System.out.println(a.getText());
+        }
+        System.out.println();
     }
+
+    private static ParseTree getParseTree(String path) throws IOException {
+        CharStream in = CharStreams.fromFileName(path);
+        Java8Lexer lexer = new Java8Lexer(in);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        Java8Parser parser = new Java8Parser(tokens);
+        return parser.compilationUnit();
+    }
+
+    static private Pa
 }
